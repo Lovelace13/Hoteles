03977954e08a04833f3fe44395a54b6e6d49c0bc
Katiuska <kmmarin@espol.edu.ec>
2021-10-08 15:28:42 -0500

    backend proyecto hoteles

--- hotel_salinas_app_movil/src/app/bookings/booking.model.ts
+++
@@ -1,9 +0,0 @@
-export class Booking {
-  constructor(
-    public id: String,
-    public placeId: String,
-    public userId: String,
-    public placeTitle: String,
-    public guestNumber: number
-  ) {}
-}
--- hotel_salinas_app_movil/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  API_URI: 'https://hotelsalinasec.cloudns.cl/'
-};
